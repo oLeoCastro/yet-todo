113990898d50aa15131fc7683a8b7708c76cba89
Leo Castro <leocastro90@outlook.com>
2022-07-28 05:07:50 -0300

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="w-full flex justify-center items-center h-20 bg-zinc-700">
+      Header
+    </div>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div className="flex w-full h-full bg-zinc-500">
+      <Header />
+    </div>
+  );
+}
