55db71f859c8c005c6ef8abd570d56f278209688
Leo Castro <leocastro90@outlook.com>
2022-07-28 09:00:42 -0300

    add

---
+++ src/components/Main.tsx
@@ -0,0 +1,3 @@
+export default function Main() {
+  return <div className="flex justify-center">Main</div>;
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import Main from "../components/Main";
 
 export default function Home() {
   return (
-    <div className="flex w-full h-full bg-zinc-500">
+    <div className="flex flex-col w-full h-full bg-zinc-500">
       <Header />
+      <Main />
     </div>
   );
 }
